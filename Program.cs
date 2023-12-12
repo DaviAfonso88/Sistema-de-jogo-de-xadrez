5198b3a70d6e220d27ec7c1bf33447cfff6a4876
Davi Afonso <daviafonso5894@gmail.com>
2023-12-11 22:02:55 -0300

    Testes iniciais com movimentos possiveis das pecas e correcao de movimentacao da torre

--- Program.cs
+++ Program.cs
@@ -11,13 +11,23 @@ namespace xadrex_console
             try
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
+
                 while (!partida.terminada)
                 {
                     Console.Clear();
+
                     Tela.imprimirTabuleiro(partida.tab);
+
                     Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicaoPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicaoPossiveis);
+
+                    Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
